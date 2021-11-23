32bd4d076c1266206616acb045e165dd74febd73
Junyu Chen <jchen245@jhmi.edu>
2021-11-23 11:42:12 -0500

    Update TransMorph_Bayes.py

--- TransMorph/models/TransMorph_Bayes.py
+++ TransMorph/models/TransMorph_Bayes.py
@@ -472,22 +472,43 @@ class PatchEmbed(nn.Module):
 
         return x
 
-class SinusoidalPositionEmbedding(nn.Module):
-    '''
-    Rotary Position Embedding
-    '''
-    def __init__(self,):
-        super(SinusoidalPositionEmbedding, self).__init__()
+class SinPositionalEncoding3D(nn.Module):
+    def __init__(self, channels):
+        """
+        :param channels: The last dimension of the tensor you want to apply pos emb to.
+        """
+        super(SinPositionalEncoding3D, self).__init__()
+        channels = int(np.ceil(channels/6)*2)
+        if channels % 2:
+            channels += 1
+        self.channels = channels
+        self.inv_freq = 1. / (10000 ** (torch.arange(0, channels, 2).float() / channels))
+        #self.register_buffer('inv_freq', inv_freq)
 
-    def forward(self, x):
-        batch_sz, n_patches, hidden = x.shape
-        position_ids = torch.arange(0, n_patches).float().cuda()
-        indices = torch.arange(0, hidden//2).float().cuda()
-        indices = torch.pow(10000.0, -2 * indices / hidden)
-        embeddings = torch.einsum('b,d->bd', position_ids, indices)
-        embeddings = torch.stack([torch.sin(embeddings), torch.cos(embeddings)], dim=-1)
-        embeddings = torch.reshape(embeddings, (1, n_patches, hidden))
-        return embeddings
+    def forward(self, tensor):
+        """
+        :param tensor: A 5d tensor of size (batch_size, x, y, z, ch)
+        :return: Positional Encoding Matrix of size (batch_size, x, y, z, ch)
+        """
+        tensor = tensor.permute(0, 2, 3, 4, 1)
+        if len(tensor.shape) != 5:
+            raise RuntimeError("The input tensor has to be 5d!")
+        batch_size, x, y, z, orig_ch = tensor.shape
+        pos_x = torch.arange(x, device=tensor.device).type(self.inv_freq.type())
+        pos_y = torch.arange(y, device=tensor.device).type(self.inv_freq.type())
+        pos_z = torch.arange(z, device=tensor.device).type(self.inv_freq.type())
+        sin_inp_x = torch.einsum("i,j->ij", pos_x, self.inv_freq)
+        sin_inp_y = torch.einsum("i,j->ij", pos_y, self.inv_freq)
+        sin_inp_z = torch.einsum("i,j->ij", pos_z, self.inv_freq)
+        emb_x = torch.cat((sin_inp_x.sin(), sin_inp_x.cos()), dim=-1).unsqueeze(1).unsqueeze(1)
+        emb_y = torch.cat((sin_inp_y.sin(), sin_inp_y.cos()), dim=-1).unsqueeze(1)
+        emb_z = torch.cat((sin_inp_z.sin(), sin_inp_z.cos()), dim=-1)
+        emb = torch.zeros((x,y,z,self.channels*3),device=tensor.device).type(tensor.type())
+        emb[:,:,:,:self.channels] = emb_x
+        emb[:,:,:,self.channels:2*self.channels] = emb_y
+        emb[:,:,:,2*self.channels:] = emb_z
+        emb = emb[None,:,:,:,:orig_ch].repeat(batch_size, 1, 1, 1, 1)
+        return emb.permute(0, 4, 1, 2, 3)
 
 class SwinTransformer(nn.Module):
     r""" Swin Transformer
@@ -560,7 +581,7 @@ class SwinTransformer(nn.Module):
             self.absolute_pos_embed = nn.Parameter(
                 torch.zeros(1, embed_dim, patches_resolution[0], patches_resolution[1], patches_resolution[2]))
             trunc_normal_(self.absolute_pos_embed, std=.02)
-        self.pos_embd = SinusoidalPositionEmbedding()
+        self.pos_embd = SinPositionalEncoding3D(96).cuda()
         self.pos_drop = nn.Dropout(p=drop_rate)
 
         # stochastic depth
@@ -647,8 +668,7 @@ class SwinTransformer(nn.Module):
             absolute_pos_embed = nnf.interpolate(self.absolute_pos_embed, size=(Wh, Ww, Wt), mode='trilinear')
             x = (x + absolute_pos_embed).flatten(2).transpose(1, 2)  # B Wh*Ww*Wt C
         elif self.spe:
-            x = x.flatten(2).transpose(1, 2)
-            x += self.pos_embd(x)
+            x = (x + self.pos_embd(x)).flatten(2).transpose(1, 2)
         else:
             x = x.flatten(2).transpose(1, 2)
         x = self.pos_drop(x)
