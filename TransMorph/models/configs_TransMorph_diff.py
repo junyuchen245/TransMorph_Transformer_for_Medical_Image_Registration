f60ce5d81acaab30269a7f9e95abb0143f571128
Junyu Chen <jchen245@jhmi.edu>
2021-12-02 14:02:28 -0500

    Update configs_TransMorph_diff.py

--- TransMorph/models/configs_TransMorph_diff.py
+++ TransMorph/models/configs_TransMorph_diff.py
@@ -48,7 +48,7 @@ def get_TransMorphDiff_config():
     config.embed_dim = 96
     config.depths = (2, 2, 4, 2)
     config.num_heads = (4, 4, 8, 8)
-    config.window_size = (5, 6, 7, 7)
+    config.window_size = (5, 6, 7)
     config.mlp_ratio = 4
     config.pat_merg_rf = 4
     config.qkv_bias = False
