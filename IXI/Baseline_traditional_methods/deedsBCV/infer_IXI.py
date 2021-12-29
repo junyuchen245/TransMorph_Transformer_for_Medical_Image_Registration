0f954b0d2cb9561d082527ec1cc3e91a4ac405ac
Junyu Chen <jchen245@jhmi.edu>
2021-12-29 15:00:18 -0500

    Update infer_IXI.py

--- IXI/Baseline_traditional_methods/deedsBCV/infer_IXI.py
+++ IXI/Baseline_traditional_methods/deedsBCV/infer_IXI.py
@@ -17,22 +17,6 @@ def nib_load(file_name):
     proxy.uncache()
     return data
 
-def detJacobian(Axij,Ayij, img_sz, **kwargs):
-    [xFX, xFY] = np.gradient(Axij)
-    [yFX, yFY] = np.gradient(Ayij)
-    jac_det = np.zeros(Axij.shape)
-    for i in range(img_sz):
-        for j in range(img_sz):
-            jac_mij = [[xFX[i, j], xFY[i, j]], [yFX[i, j], yFY[i, j]]]
-            jac_det[i, j] =  np.linalg.det(jac_mij)
-    return jac_det
-
-def plot_grid(gridx,gridy, **kwargs):
-    for i in range(gridx.shape[1]):
-        plt.plot(gridx[i,:], gridy[i,:], linewidth=0.8, **kwargs)
-    for i in range(gridx.shape[0]):
-        plt.plot(gridx[:,i], gridy[:,i], linewidth=0.8, **kwargs)
-
 class AverageMeter(object):
     """Computes and stores the average and current value"""
     def __init__(self):
@@ -53,12 +37,6 @@ class AverageMeter(object):
         self.avg = self.sum / self.count
         self.vals.append(val)
         self.std = np.std(self.vals)
-
-def MSE_torch(x, y):
-    return np.mean((x - y) ** 2)
-
-def MAE_torch(x, y):
-    return np.mean(np.abs(x - y))
 
 def main():
     atlas_dir = 'Path_to_IXI_data/atlas.pkl'
@@ -123,72 +101,6 @@ def csv_writter(line, name):
     with open(name+'.csv', 'a') as file:
         file.write(line)
         file.write('\n')
-
-def visual_flow(flow, down = 7):
-    flow = flow.detach().cpu().numpy()[0, :, :, :, :]
-    fig = plt.figure()
-    ax = fig.gca(projection='3d')
-    x, y, z = np.meshgrid(np.arange(0, flow.shape[2], 1),
-                          np.arange(0, flow.shape[1], 1),
-                          np.arange(0, flow.shape[3], 1))
-    print(x.shape)
-
-    xdown = x[0:-1:down, 0:-1:down, 0:-1:down]
-    ydown = y[0:-1:down, 0:-1:down, 0:-1:down]
-    zdown = z[0:-1:down, 0:-1:down, 0:-1:down]
-    u = flow[0, :, :, :]
-    udown = u[0:-1:down, 0:-1:down, 0:-1:down]
-    print(u.shape)
-    v = flow[1, :, :, :]
-    vdown = v[0:-1:down, 0:-1:down, 0:-1:down]
-    w = flow[2, :, :, :]
-    wdown = w[0:-1:down, 0:-1:down, 0:-1:down]
-
-    ax.quiver(xdown, ydown, zdown, udown, vdown, wdown, length=2, linewidths=1)
-    plt.show()
-
-def comput_fig(img):
-    img = img.detach().cpu().numpy()[0, 0, 16:32, :, :]
-    fig = plt.figure(figsize=(12,12), dpi=180)
-    for i in range(img.shape[0]):
-        plt.subplot(4, 4, i + 1)
-        plt.axis('off')
-        plt.imshow(img[i, :, :], cmap='gray')
-    fig.subplots_adjust(wspace=0, hspace=0)
-    return fig
-
-def display_deformed_grid(flow, img_sz=160, down=7):
-    flow = flow.detach().cpu().numpy()[0, :, 76:80, :, :]
-    fig = plt.figure(figsize=(12, 12), dpi=200)
-    for idx in range(flow.shape[1]):
-        x = np.arange(0, img_sz, 1)
-        y = np.arange(0, img_sz, 1)
-        X, Y = np.meshgrid(x, y)
-        u =flow[1, idx, :, :].reshape(img_sz, img_sz)
-        v =flow[2, idx, :, :].reshape(img_sz, img_sz)
-        #print('max u: '+str(np.max(u)))
-
-        phix = X; phiy = Y
-        for i in range(0, img_sz):
-            for j in range(0, img_sz):
-                # add the displacement for each p(k) in the sum
-                phix[i, j] = phix[i, j] - u[i, j]
-                phiy[i, j] = phiy[i, j] - v[i, j]
-        phixdown = phix[0:-1:down, 0:-1:down]
-        phiydown = phiy[0:-1:down, 0:-1:down]
-        ax = plt.subplot(2, 2, idx + 1)
-        ax.set_aspect('equal', adjustable='box')
-        plt.axis('off')
-        plot_grid(phixdown,phiydown, color="k")
-        plt.gca().invert_yaxis()
-        #plt.subplot(1, 7, 2)
-        #detJac = detJacobian(phix, phiy, img_sz)
-        #print('Min det(Jac): '+str(np.min(np.abs(detJac))))
-        #print('# det(Jac)<=0: ' + str((detJac == 0).sum()))
-        #plt.imshow(detJac); plt.title('det(Jacobian)')
-        #plt.colorbar()
-    plt.show()
-    return fig
 
 if __name__ == '__main__':
     '''
