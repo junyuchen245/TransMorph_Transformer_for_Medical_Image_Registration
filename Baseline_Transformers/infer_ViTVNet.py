0889e29e49da14793e9b5610b4c7da72b4db9eec
Junyu Chen <jchen245@jhmi.edu>
2022-09-13 12:30:18 -0400

    Create infer_ViTVNet.py

---
+++ Baseline_Transformers/infer_ViTVNet.py
@@ -0,0 +1,139 @@
+import glob
+import os, losses, utils
+from torch.utils.data import DataLoader
+from data import datasets, trans
+import numpy as np
+import torch
+from torchvision import transforms
+import matplotlib.pyplot as plt
+from natsort import natsorted
+from models.ViTVNet import CONFIGS as CONFIGS_ViT
+from models.ViTVNet import ViTVNet
+import torch.nn as nn
+
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+        self.vals = []
+        self.std = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+        self.vals.append(val)
+        self.std = np.std(self.vals)
+
+def MSE_torch(x, y):
+    return torch.mean((x - y) ** 2)
+
+def MAE_torch(x, y):
+    return torch.mean(torch.abs(x - y))
+
+def main():
+    atlas_dir = 'D:/DATA/IXI_brain/network_training/atlas.pkl'
+    test_dir = 'D:/DATA/IXI_brain/network_training/Test/'
+    model_idx = -1
+    weights = [1, 1]
+    model_folder = 'ViTVNet_ncc_{}_diffusion_{}/'.format(weights[0], weights[1])
+    model_dir = 'experiments/' + model_folder
+    dict = utils.process_label()
+    if not os.path.exists('Quantitative_Results/'):
+        os.makedirs('Quantitative_Results/')
+    if os.path.exists('Quantitative_Results/' + model_folder[:-1] + '.csv'):
+        os.remove('Quantitative_Results/' + model_folder[:-1] + '.csv')
+    csv_writter(model_folder[:-1], 'Quantitative_Results/' + model_folder[:-1])
+    line = ''
+    for i in range(46):
+        line = line + ',' + dict[i]
+    csv_writter(line + ',' + 'non_jec', 'Quantitative_Results/' + model_folder[:-1])
+    config_vit = CONFIGS_ViT['ViT-V-Net']
+    img_size = (160, 192, 224)
+    model = ViTVNet(config_vit, img_size=img_size)
+    best_model = torch.load(model_dir + natsorted(os.listdir(model_dir))[model_idx])['state_dict']
+    print('Best model: {}'.format(natsorted(os.listdir(model_dir))[model_idx]))
+    model.load_state_dict(best_model)
+    model.cuda()
+    reg_model = utils.register_model(img_size, 'bilinear').cuda()
+    for param in reg_model.parameters():
+        param.requires_grad = False
+        param.volatile = True
+    test_composed = transforms.Compose([trans.Seg_norm(),
+                                        trans.NumpyType((np.float32, np.int16)),
+                                        ])
+    test_set = datasets.IXIBrainInferDataset(glob.glob(test_dir + '*.pkl'), atlas_dir, transforms=test_composed)
+    test_loader = DataLoader(test_set, batch_size=1, shuffle=False, num_workers=1, pin_memory=True, drop_last=True)
+    eval_dsc_def = AverageMeter()
+    eval_dsc_raw = AverageMeter()
+    eval_det = AverageMeter()
+    with torch.no_grad():
+        stdy_idx = 0
+        for data in test_loader:
+            model.eval()
+            data = [t.cuda() for t in data]
+            x = data[0]
+            y = data[1]
+            x_seg = data[2]
+            y_seg = data[3]
+            x_in = torch.cat((x, y), dim=1)
+            output = model(x_in)
+            flow = output[1]
+            x_seg_oh = nn.functional.one_hot(x_seg.long(), num_classes=46)
+            x_seg_oh = torch.squeeze(x_seg_oh, 1)
+            x_seg_oh = x_seg_oh.permute(0, 4, 1, 2, 3).contiguous()
+            # x_segs = model.spatial_trans(x_seg.float(), flow.float())
+            x_segs = []
+            for i in range(46):
+                def_seg = reg_model([x_seg_oh[:, i:i + 1, ...].float(), flow.float()])
+                x_segs.append(def_seg)
+            x_segs = torch.cat(x_segs, dim=1)
+            def_out = torch.argmax(x_segs, dim=1, keepdim=True)
+            del x_segs, x_seg_oh
+            tar = y.detach().cpu().numpy()[0, 0, :, :, :]
+            jac_det = utils.jacobian_determinant_vxm(flow.detach().cpu().numpy()[0, :, :, :, :])
+            line = utils.dice_val_substruct(def_out.long(), y_seg.long(), stdy_idx)
+            line = line +','+str(np.sum(jac_det <= 0)/np.prod(tar.shape))
+            csv_writter(line, 'Quantitative_Results/' + model_folder[:-1])
+            eval_det.update(np.sum(jac_det <= 0) / np.prod(tar.shape), x.size(0))
+
+            dsc_trans = utils.dice_val(def_out.long(), y_seg.long(), 46)
+            dsc_raw = utils.dice_val(x_seg.long(), y_seg.long(), 46)
+            print('Trans dsc: {:.4f}, Raw dsc: {:.4f}'.format(dsc_trans.item(),dsc_raw.item()))
+            eval_dsc_def.update(dsc_trans.item(), x.size(0))
+            eval_dsc_raw.update(dsc_raw.item(), x.size(0))
+            stdy_idx += 1
+
+        print('Deformed DSC: {:.3f} +- {:.3f}, Affine DSC: {:.3f} +- {:.3f}'.format(eval_dsc_def.avg,
+                                                                                    eval_dsc_def.std,
+                                                                                    eval_dsc_raw.avg,
+                                                                                    eval_dsc_raw.std))
+        print('deformed det: {}, std: {}'.format(eval_det.avg, eval_det.std))
+
+def csv_writter(line, name):
+    with open(name+'.csv', 'a') as file:
+        file.write(line)
+        file.write('\n')
+
+if __name__ == '__main__':
+    '''
+    GPU configuration
+    '''
+    GPU_iden = 1
+    GPU_num = torch.cuda.device_count()
+    print('Number of GPU: ' + str(GPU_num))
+    for GPU_idx in range(GPU_num):
+        GPU_name = torch.cuda.get_device_name(GPU_idx)
+        print('     GPU #' + str(GPU_idx) + ': ' + GPU_name)
+    torch.cuda.set_device(GPU_iden)
+    GPU_avai = torch.cuda.is_available()
+    print('Currently using: ' + torch.cuda.get_device_name(GPU_iden))
+    print('If the GPU is available? ' + str(GPU_avai))
+    main()
