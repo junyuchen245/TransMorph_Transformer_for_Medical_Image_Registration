c145615cedd3e33009ae3398a9d7677d8a0d94b2
Junyu Chen <jchen245@jhmi.edu>
2024-09-25 15:25:20 -0400

    Update test.sh

--- Docker/test.sh
+++ Docker/test.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-docker pull jchen245/transmorph_brain_mri_registration:transmorph_brain_mri_t1_v0
+docker pull jchen245/transmorph_brain_mri_registration:transmorph_brain_mri_t1_v2
 
 docker run --rm  \
         --ipc=host \
@@ -8,5 +8,5 @@ docker run --rm  \
         --mount type=bind,source=/scratch/jchen/python_projects/TransMorph_brain_registration/configs_registration.json,target=/configs_registration.json \
         --mount type=bind,source=/scratch/jchen/python_projects/TransMorph_brain_registration/test_data,target=/input \
         --mount type=bind,source=/scratch/jchen/python_projects/TransMorph_brain_registration/test_output,target=/output \
-        jchen245/transmorph_brain_mri_registration:transmorph_brain_mri_t1_v0
+        jchen245/transmorph_brain_mri_registration:transmorph_brain_mri_t1_v2
 
